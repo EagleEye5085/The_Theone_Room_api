9d88b09c71385e6e0a58fab339c105a3e5a24b77
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-15 15:31:06 -0600

    add review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,14 @@
 class Review < ApplicationRecord
 
-  belongs_to :users
-  belongs_to :throne_rooms
+  validates_presence_of :cleanliness
+  validates_presence_of :ambiance
+  validates_presence_of :tp_quality
+  validates_presence_of :privacy
+  validates_presence_of :other_comments
+  validates_presence_of :throne_room_id
+  validates_presence_of :user_id
+
+  # belongs_to :user
+  belongs_to :throne_room
 
 end

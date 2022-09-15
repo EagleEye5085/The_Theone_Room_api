a8b595b98998c1c8e783aed2e705df73597b9cb5
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-15 15:29:54 -0600

    add/change data

--- db/schema.rb
+++ db/schema.rb
@@ -20,19 +20,18 @@ ActiveRecord::Schema.define(version: 2022_09_14_210909) do
     t.integer "ambiance"
     t.integer "tp_quality"
     t.integer "privacy"
+    t.integer "user_id"
     t.string "other_comments"
     t.bigint "throne_room_id"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["throne_room_id"], name: "index_reviews_on_throne_room_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "throne_rooms", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.float "lattitude"
+    t.float "latitude"
     t.float "longitude"
     t.string "directions"
     t.boolean "baby_changing_station"
@@ -50,5 +49,4 @@ ActiveRecord::Schema.define(version: 2022_09_14_210909) do
   end
 
   add_foreign_key "reviews", "throne_rooms"
-  add_foreign_key "reviews", "users"
 end

8152564953f60cddd2331bfde1b108f9582c53d9
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-21 09:53:38 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'throne_rooms/closest_five', to: 'throne_rooms#closest_five'
       get 'reviews/recent', to: 'reviews#recent'
+      get 'throne_rooms/reviews/review_averages', to: 'throne_rooms#review_averages'
       resources :reviews
       resources :throne_rooms
     end
